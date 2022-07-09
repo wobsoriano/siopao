bf3cc7d4c06bb25a68405f5a26e1c26d5674da38
Robert Soriano <sorianorobertc@gmail.com>
2022-07-08 19:51:16 -0700

    update playground

--- playground.ts
+++ playground.ts
@@ -1,35 +1,4 @@
-import { createRouter } from "radix3"
-import type { RadixRouter } from "radix3"
-
-class App {
-  router: RadixRouter
-
-  constructor() {
-    this.router = createRouter()
-  }
-
-  use(path: string, handler: () => Response) {
-    this.router.insert(path, {
-      handler
-    })
-  }
-
-  fetch(request: Request) {
-    const { pathname } = new URL(request.url)
-
-    const result = this.router.lookup(pathname)
-
-    if (result) {
-      return result.handler() as Response
-    }
-
-    return new Response('Not found', {
-      status: 400
-    })
-  }
-}
-
-// import Siopao from 'siopao'
+import { App } from './src'
 
 const app = new App()
 
