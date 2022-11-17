88768e726762486f4c06237f8ba3b159319e8f38
wobsoriano <sorianorobertc@gmail.com>
2022-11-17 09:56:50 -0800

    fix tests

--- test/siopao.test.ts
+++ test/siopao.test.ts
@@ -10,7 +10,7 @@ describe('Siopao', () => {
     let server: Server
     try {
       server = app.serve()
-      const response = await fetch('http://localhost:3000/ping')
+      const response = await fetch('http://0.0.0.0:3000/ping')
       expect(response.status).toBe(200)
       expect(await response.text()).toBe('pong')
     } catch (e) {
@@ -24,7 +24,7 @@ describe('Siopao', () => {
     let server: Server
     try {
       server = app.serve()
-      const response = await fetch('http://localhost:3000/404')
+      const response = await fetch('http://0.0.0.0:3000/404')
       expect(response.status).toBe(404)
       expect(await response.text()).toBe('Not Found')
     } catch (e) {
@@ -38,7 +38,7 @@ describe('Siopao', () => {
     let server: Server
     try {
       server = app.serve()
-      const response = await fetch('http://localhost:3000/ping', { method: 'POST' })
+      const response = await fetch('http://0.0.0.0:3000/ping', { method: 'POST' })
       expect(response.status).toBe(405)
       expect(await response.text()).toBe('Method Not Allowed')
     } catch (e) {
